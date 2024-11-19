b2dca4ca1e0cba4f20cdb6dee818ec8c17cd3f25
Ivan Brunetto <ivanbrunetto@gmail.com>
2024-11-19 06:40:21 -0300

    swiper carousel

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,21 +1,67 @@
 import { connect } from "react-redux";
 import PollCard from "./PollCard";
 import "./Dashboard.css";
+import { useState, useEffect } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { register } from "swiper/element/bundle";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+register();
 
 const Dashboard = (props) => {
+  const [slidesPerView, setSlidesPerView] = useState(3);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 960) {
+        setSlidesPerView(3);
+      } else if (window.innerWidth < 700) {
+        setSlidesPerView(1);
+      } else {
+        setSlidesPerView(2);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="dashboard">
       <h2>Unanswered Polls</h2>
       <div className="poll-list-container">
-        {props.uQuestions.map((id) => (
-          <PollCard id={id} />
-        ))}
+        <Swiper
+          slidesPerView={slidesPerView}
+          navigation
+          pagination={{ clickable: true }}
+        >
+          {props.uQuestions.map((id) => (
+            <SwiperSlide key={id}>
+              <PollCard id={id} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
       <h2>Answered Polls</h2>
       <div className="poll-list-container">
-        {props.aQuestions.map((id) => (
-          <PollCard id={id} />
-        ))}
+        <Swiper
+          slidesPerView={slidesPerView}
+          pagination={{ clickable: true }}
+          navigation
+        >
+          {props.aQuestions.map((id) => (
+            <SwiperSlide key={id}>
+              <PollCard id={id} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
